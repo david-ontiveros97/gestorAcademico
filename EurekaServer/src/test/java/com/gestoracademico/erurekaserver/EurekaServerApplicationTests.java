c8f9a3246a321902e2ce65ea8771679f6525d86f
david-ontiveros97 <david.oivd911@gmail.com>
2023-11-05 20:44:18 -0600

    feat: Agregar microservicio Eureka

---
+++ EurekaServer/src/test/java/com/gestoracademico/erurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestoracademico.erurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
